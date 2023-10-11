e05ca23da7359bef4b283a0556fa30262c7da9c5
LBorgess <lucas_rancer@hotmail.com>
2023-10-10 21:15:24 -0300

    teste do menu dropdown

--- app/src/main/java/br/com/etecia/gorjeta/MainActivity.java
+++ app/src/main/java/br/com/etecia/gorjeta/MainActivity.java
@@ -3,12 +3,26 @@ package br.com.etecia.gorjeta;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
+
+    Spinner menu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        menu = (Spinner) findViewById(R.id.menu_valores);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.menu_valores, android.R.layout.simple_spinner_item);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        menu.setAdapter(adapter);
+
     }
 }
